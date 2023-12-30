7b2fce6b27ecd612a5a79291ac2108b1d44f2f54
Babar Ali <babaraliengineer32@gmail.com>
2023-12-30 21:17:54 +0500

    double player two

--- src/player.java
+++ src/player.java
@@ -5,6 +5,9 @@ public class player {
         //sum of two numbers
         int c=a+b;
         int d=a-b;
+        double num1=2;
+        double num2=3;
+        double num3=num1/num2;
         System.out.println("sum "+c+" subtraction player one "+d);
         
 
