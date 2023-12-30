f336601c7aaea5b0ecb906a8c90e4fda53832fcf
Babar Ali <babaraliengineer32@gmail.com>
2023-12-30 20:18:29 +0500

    parent branch just sum of two numbers code

---
+++ src/player.java
@@ -0,0 +1,13 @@
+public class player {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+        //sum of two numbers
+        int c=a+b;
+        System.out.println(c);
+        
+
+
+
+    }
+}
