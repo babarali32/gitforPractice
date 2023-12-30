5b1405528e8d50f39127e45f9da259b4c4d69981
Babar Ali <babaraliengineer32@gmail.com>
2023-12-30 21:20:22 +0500

    player one added spaces

--- src/player.java
+++ src/player.java
@@ -3,6 +3,8 @@ public class player {
         int a=2;
         int b=4;
         //sum of two numbers
+
+        
         int c=a+b;
         int z=a*b;
         System.out.println("sum "+c+" player 2 added multiply "+z);
